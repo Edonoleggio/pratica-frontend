9195eb75199337b62dc429a5a0eafc47d18d544b
Edonoleggio <edonoleggiolampedusa@gmail.com>
2026-05-11 22:51:54 +0200

    Add CSS import and render method in main.jsx

--- main.jsx
+++ main.jsx
@@ -1,3 +1,13 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './pratica-app-v8.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pratica-app-v8.jsx'
