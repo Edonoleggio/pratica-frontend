f266ff35277bb99a8139e9419ec583311690a5f6
Edonoleggio <edonoleggiolampedusa@gmail.com>
2026-05-19 14:00:56 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './pratica-app-v8.jsx'
+import App from './pratica-app-final.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
