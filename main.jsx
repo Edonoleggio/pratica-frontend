ccb11217c8afc976c331e48d49189c9e62344e51
Edonoleggio <edonoleggiolampedusa@gmail.com>
2026-05-19 15:06:37 +0200

    Add index.css import

--- main.jsx
+++ main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './pratica-app-final.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
